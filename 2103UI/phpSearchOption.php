5afd2f4b9b73ab74bc9f6fcc4ec1ecdbba002aff
gouhang26 <gouhang2@gmail.com>
2021-11-24 18:22:09 +0800

    UI change now index is the UI page

--- 2103UI/phpSearchOption.php
+++ 2103UI/phpSearchOption.php
@@ -1,8 +1,23 @@
-<?php
+<head>
+    <?php include 'head.php'; ?>
+    <title>This is data</title>
+    <style>
+.redcolorselect {
+    background-color: #E60000;
+    
+}
+.greencolorselect{
+    background-color: #33CC33;
+}
 
-$search = $_POST['search'];
-$column = $_POST['column'];
+</style>
 
+</head>
+
+<body>
+    <?php 
+
+ 
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -18,18 +33,65 @@ $sql = "SELECT student.Student_ID, student.Student_Name
 FROM student
 INNER JOIN student_has ON student.Student_ID = student_has.Student_ID
 INNER JOIN healthstatus ON student_has.HealthStatus_ID = healthstatus.HealthStatus_ID
-WHERE healthstatus.CovidPositive = 1;";
+WHERE healthstatus.CovidPositive = 1;";   //change query here
 
 $result = $conn->query($sql);
+    ?>
 
-if ($result->num_rows > 0){
-while($row = $result->fetch_assoc() ){
-	echo $row["Student_ID"]."  ".$row["Student_Name"]."  <br>";
-}
-} else {
-	echo "0 records";
-}
+    <div class="container">
+        <h1>data present</h1>
+        <div>
+            
+            <table class="table table-striped" id="myTable">
+                <thead class="thead-light">
+                    <tr class="table-dark">
+                    <th scope="col">Student name</th>
+                    <th scope="col">Infectious time</th>
+                    <th scope="col">student id</th>
+                    <th scope="col">course</th>
+                    <th scope="col">time he has</th>
+                  
+                    
+                    
+                    </tr>
+                </thead>
 
-$conn->close();
+                <tbody>
 
+                <?php
+                if ($result->num_rows > 0){
+					while($row = $result->fetch_assoc() ){
+                    echo "<tr>
+                        <td>" . $row['Student_ID'] . "</td>
+                        <td>" . $row['Student_Name'] . "</td>
+						<td>" . $row['Student_ID'] . "</td>
+                        <td>" . $row['Student_Name'] . "</td>
+						<td>" . $row['Student_ID'] . "</td>
+                      
+                      
+                     
+                        </tr>";
+                }}else {
+					echo "0 records";
+				}
+                ?>
+                </tbody>
+            </table>
+        </div>
+        
+    </div>
+	<?php
+    $conn->close();
 ?>
+    <script>
+        $(document).ready( function () {
+            $('#myTable').DataTable();
+        });
+    </script>
+
+
+    </body>
+</html>
+
+
+
