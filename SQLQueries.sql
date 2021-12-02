30b70a85666f10806a66ce60544841a4d200ed3d
Jia Cheng <dazzed17@gmail.com>
2021-12-02 23:15:11 +0800

    updated sqlqueries to better reflect functions

--- SQLQueries.sql
+++ SQLQueries.sql
@@ -1,11 +1,12 @@
--- Retrieving Students currently studying in SIT with that are Covid Positive (lol everyone has covid)
+-- Retrieving Students currently studying in SIT with that are Covid Positive
+-- BUTTON in UI: Student with COVID
 SELECT student.Student_ID, student.Student_Name
 FROM student
 INNER JOIN student_has ON student.Student_ID = student_has.Student_ID
 INNER JOIN healthstatus ON student_has.HealthStatus_ID = healthstatus.HealthStatus_ID
 WHERE healthstatus.CovidPositive = 1;
 
--- Find all students that are covidpositive 
+-- Find all students that are covidpositive (NESTED; FOR SPEED COMPARISON AGAINST PRVEIOUS QUERY)
 SELECT student.Student_ID, student.Student_Name 
 FROM student 
 WHERE student.Student_ID 
@@ -17,27 +18,29 @@ IN(
 	WHERE healthstatus.CovidPositive=1
 	);
 
--- Retrieving Profs currently teaching in SIT that are Covid Positive (everyone has covid here lol)
+-- Retrieving Profs currently teaching in SIT that are Covid Positive
+-- BUTTON in UI: Professor with COVID
 SELECT professor.Professor_ID, professor.Professor_Name
 FROM professor
 INNER JOIN professor_has ON professor.Professor_ID = professor_has.Professor_ID
 INNER JOIN healthstatus ON professor_has.HealthStatus_ID = healthstatus.HealthStatus_ID
 WHERE healthstatus.CovidPositive = 1;
 
--- Students that are covid positive
+-- Students who are close contact with covid-positive individuals
+-- BUTTON in UI: High-Risk Individual
 SELECT student.Student_ID, student.Student_Name
 FROM student
 INNER JOIN student_has ON student.Student_ID = student_has.Student_ID
 INNER JOIN healthstatus ON student_has.HealthStatus_ID = healthstatus.HealthStatus_ID
 WHERE healthstatus.CovidPositive = 1
 UNION
--- The lesson that they have went to
 SELECT student.Student_ID, student.Student_Name
 FROM student
 INNER JOIN attend ON student.Student_ID = attend.Student_ID
-INNER JOIN lesson ON attend.Lesson_ID = lesson.Lesson_ID; -- Can add a between clause here for the date range you want to find
+INNER JOIN lesson ON attend.Lesson_ID = lesson.Lesson_ID;
 
 -- Find all lessons that had infected students
+-- BUTTON in UI: Lesson with COVID
 SELECT DISTINCT attend.Lesson_ID 
 FROM attend 
 WHERE attend.Student_ID 
@@ -50,6 +53,7 @@ IN(
 	);
 
 -- Find all students whose daily health declaration temperature taking is higher than 37.5
+-- BUTTON in UI: Abnormal Temperature Student
 SELECT student.Student_ID, student.Student_Name 
 FROM student 
 WHERE student.Student_ID 
@@ -62,6 +66,7 @@ IN(
 	);
 
 -- Find all classrooms that had infected students
+-- BUTTON in UI: Classroom with COVID
 SELECT * FROM classroom 
 WHERE classroom.Classroom_ID 
 IN(
@@ -82,6 +87,7 @@ IN(
 				);
 
 -- Count the total number of students with covid 
+-- BUTTON in UI: Total students with COVID
 SELECT count(student.Student_ID) As StudentsWithCovid
 FROM student
 INNER JOIN student_has ON student.Student_ID = student_has.Student_ID
@@ -89,6 +95,7 @@ INNER JOIN healthstatus ON student_has.HealthStatus_ID = healthstatus.HealthStat
 WHERE healthstatus.CovidPositive = 1;
 
 -- Count the total number of professors with covid
+-- BUTTON in UI: Total profs with COVID
 SELECT count(professor.Professor_ID) As ProfessorsWithCovid
 FROM professor
 INNER JOIN professor_has ON professor.Professor_ID = professor_has.Professor_ID
