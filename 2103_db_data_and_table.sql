c806fbcc71ff16fea36d5a3ff16427c402514cd4
Jia Cheng <dazzed17@gmail.com>
2021-12-02 22:46:55 +0800

    merged insert queries

--- 2103_db_data_and_table.sql
+++ 2103_db_data_and_table.sql
@@ -414,6 +414,37 @@ insert into dailyhealthdeclaration (healthDeclaration_ID,Temperature,DateTaken) 
 insert into dailyhealthdeclaration (healthDeclaration_ID,Temperature,DateTaken) values (241021268,'36.1','2021-01-11');
 insert into dailyhealthdeclaration (healthDeclaration_ID,Temperature,DateTaken) values (241021269,'35.8','2021-01-11');
 insert into dailyhealthdeclaration (healthDeclaration_ID,Temperature,DateTaken) values (241021270,'37.5','2021-01-11');
+-- prof DHD
+insert into dailyhealthdeclaration
+values(241021911,'37.5','2021-01-14');
+insert into dailyhealthdeclaration
+values(241021912,'37.1','2021-01-14');
+insert into dailyhealthdeclaration
+values(241021913,'37.3','2021-01-14');
+insert into dailyhealthdeclaration
+values(241021914,'37.3','2021-01-14');
+insert into dailyhealthdeclaration
+values(241021915,'36.3','2021-01-14');
+insert into dailyhealthdeclaration
+values(241021916,'36.4','2021-01-14');
+insert into dailyhealthdeclaration
+values(241021917,'36.9','2021-01-14');
+insert into dailyhealthdeclaration
+values(241021918,'36.8','2021-01-14');
+insert into dailyhealthdeclaration
+values(241021919,'36.7','2021-01-14');
+insert into dailyhealthdeclaration
+values(241021920,'37.6','2021-01-14');
+insert into dailyhealthdeclaration
+values(241021921,'37.2','2021-01-14');
+insert into dailyhealthdeclaration
+values(241021922,'37.3','2021-01-14');
+insert into dailyhealthdeclaration
+values(241021923,'37.1','2021-01-14');
+insert into dailyhealthdeclaration
+values(241021924,'37.0','2021-01-14');
+insert into dailyhealthdeclaration
+values(241021925,'36.7','2021-01-14');
 
 /****************** end insert statement for dailyhealthdeclaration ************/
 /****************** insert statement for classroom ************/
