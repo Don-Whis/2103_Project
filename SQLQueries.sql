30a5f442a3878c073f9be864522a0e0b240ab9b3
Chek Wee <51693659+ZevQuek@users.noreply.github.com>
2021-12-05 12:09:41 +0800

    Update SQLQueries.sql
    added aggregate, update and delete queries

--- SQLQueries.sql
+++ SQLQueries.sql
@@ -28,19 +28,6 @@ INNER JOIN professor_has ON professor.Professor_ID = professor_has.Professor_ID
 INNER JOIN healthstatus ON professor_has.HealthStatus_ID = healthstatus.HealthStatus_ID
 WHERE healthstatus.CovidPositive = 1;
 
--- Students who are close contact with covid-positive individuals
--- BUTTON in UI: High-Risk Individual
-SELECT student.Student_ID, student.Student_Name
-FROM student
-INNER JOIN student_has ON student.Student_ID = student_has.Student_ID
-INNER JOIN healthstatus ON student_has.HealthStatus_ID = healthstatus.HealthStatus_ID
-WHERE healthstatus.CovidPositive = 1
-UNION
-SELECT student.Student_ID, student.Student_Name
-FROM student
-INNER JOIN attend ON student.Student_ID = attend.Student_ID
-INNER JOIN lesson ON attend.Lesson_ID = lesson.Lesson_ID;
-
 -- Find all lessons that had infected students
 -- BUTTON in UI: Lesson with COVID
 SELECT DISTINCT attend.Lesson_ID 
@@ -103,3 +90,43 @@ FROM professor
 INNER JOIN professor_has ON professor.Professor_ID = professor_has.Professor_ID
 INNER JOIN healthstatus ON professor_has.HealthStatus_ID = healthstatus.HealthStatus_ID
 WHERE healthstatus.CovidPositive = 1;
+
+-- Count the number of lessons that is taught by professors
+-- BUTTON in UI: NULL
+SELECT professor_ID,count(Lesson_ID) 
+AS NumberOfLessonsTaught 
+FROM 2103_tabledata.teach_by 
+GROUP BY professor_ID;
+
+
+-- Count the number of lessons that is attended by students
+-- BUTTON in UI: NULL
+SELECT Student_ID,count(Lesson_ID) 
+AS NumberOfLessonsAttended 
+FROM 2103_tabledata.attend teach_by 
+GROUP BY student_ID;
+
+-- Average age of students affected by covid
+-- BUTTON in UI: NULL
+SELECT AVG(age)
+FROM 2103_tabledata.student
+INNER JOIN 2103_tabledata.student_has ON 2103_tabledata.student_has.Student_ID = 2103_tabledata.student.Student_ID
+INNER JOIN 2103_tabledata.healthstatus ON 2103_tabledata.student_has.HealthStatus_ID = 2103_tabledata.healthstatus.HealthStatus_ID
+WHERE 2103_tabledata.healthstatus.CovidPositive = 1;
+
+-- Amount of covid positive cases
+-- BUTTON in UI: NULL
+SELECT SUM(CovidPositive)
+FROM 2103_tabledata.healthstatus
+WHERE CovidPositive = 1;
+
+-- Update covid positive of a particular student/professor
+-- BUTTON in UI: NULL
+UPDATE 2103_tabledata.healthstatus
+SET 2103_tabledata.healthstatus.CovidPositive = 1
+WHERE 2103_tabledata.healthstatus.HealthStatus_ID = 210001;
+
+-- Delete the daily health declaration data that is before january 2021
+-- BUTTON in UI: NULL
+DELETE FROM dailyhealthdeclaration 
+WHERE DateTaken < '2021-01-01';
